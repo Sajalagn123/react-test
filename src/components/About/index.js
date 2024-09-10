cd84003a5b67e7a69f9a54fc98f6cafb596d3a35
Sajal Agnihotri <sajalagn3@gmail.com>
2024-09-09 17:15:47 -0700

    test

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -12,7 +12,7 @@ function About() {
         My name is Sajal Agnihotri and I am full stack web developer from Orange Country, California. I have learned to be proficient in a wide array of front and back end programing languages, including HTML, CSS, Javascript, MySQL, NodeJS, ExpressJS, etc. In addition, I have had experience creating various apps and websites, experiences which have helped me learn and grow as a developer.
         </p>
         <p>
-        In a word becoming more increasingly reliant on technology, I plan to use my development knowledge to create and design websites that are user friendly, well designed, and useful to others. My hope is that what I create will aid people in having an easy and realxing experience, whether it's because they are entertained by my website or find it useful.
+        In a world becoming more increasingly reliant on technology, I plan to use my development knowledge to create and design websites that are user friendly, well designed, and useful to others. My hope is that what I create will aid people in having an easy and realxing experience, whether it's because they are entertained by my website or find it useful.
         </p>
       </div>
     </section>
